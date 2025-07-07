f516ae40b03233a35d176f9a04d188658c4fc282
SarahMuth <sarahmuth9@gmail.com>
2025-07-07 13:45:57 +0200

    minor adjustment

--- scripts/cooper/revision/run_az_evaluation.py
+++ scripts/cooper/revision/run_az_evaluation.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from glob import glob
 
 import pandas as pd
 from common import get_paths, get_file_names, ALL_NAMES
@@ -14,17 +15,37 @@ def run_az_evaluation(args):
     results = []
     for dataset in args.datasets:
         print(dataset, ":")
-        file_names = get_file_names(dataset, split_folder, split_names=["test"])
+        if args.in_path:
+            file_paths = glob(os.path.join(args.in_path, dataset, "*.h5"))
+            file_names = [os.path.basename(path) for path in file_paths]
+        else:
+            file_names = get_file_names(dataset, split_folder, split_names=["test"])
         seg_paths, gt_paths = get_paths(dataset, file_names)
         result = az_evaluation(
-            seg_paths, gt_paths, seg_key=seg_key, gt_key="/labels/az_merged",
+            seg_paths, gt_paths, seg_key=seg_key, gt_key="/labels/az_merged_v6",
             criterion=args.criterion, dataset=[dataset] * len(seg_paths), threshold=args.threshold,
         )
         results.append(result)
 
     results = pd.concat(results)
     output_path = f"/user/muth9/u12095/synapse-net/scripts/cooper/revision/evaluation_results/v{args.version}.xlsx"
-    results.to_excel(output_path, index=False)
+
+    if os.path.exists(output_path):
+        # Read existing data
+        existing = pd.read_excel(output_path)
+
+        # Ensure consistent column naming and types
+        if "tomo_name" in results.columns and "tomo_name" in existing.columns:
+            # Drop existing entries with matching "tomo_name"
+            existing = existing[~existing["tomo_name"].isin(results["tomo_name"])]
+
+        # Combine: old (filtered) + new
+        combined = pd.concat([existing, results], ignore_index=True)
+    else:
+        combined = results
+
+    # Save back to Excel
+    combined.to_excel(output_path, index=False)
 
 
 def visualize_az_evaluation(args):
@@ -44,7 +65,7 @@ def visualize_az_evaluation(args):
             with open_file(seg_path, "r") as f:
                 seg = f[seg_key][:].squeeze()
             with open_file(gt_path, "r") as f:
-                gt = f["/labels/az_merged"][:]
+                gt = f["/labels/az_merged_v6"][:]
 
             seg = seg > args.threshold
 
@@ -66,6 +87,7 @@ def main():
     parser.add_argument("--datasets", nargs="+", default=ALL_NAMES)
     # Set the threshold to None if the AZ prediction already a segmentation.
     parser.add_argument("--threshold", type=float, default=0.5)
+    parser.add_argument("--in_path", "-i", default=None)
     args = parser.parse_args()
 
     if args.visualize:
